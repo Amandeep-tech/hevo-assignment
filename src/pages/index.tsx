d260f9556fdedd5393303f42f1ad44433a2abdc0
Amandeep Singh <amandeep.s@zolostays.com>
2025-05-22 10:07:42 +0530

    Update Home component styling to adjust padding for responsive design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const poppins = Poppins({
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${poppins.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between md:p-24 p-1 ${poppins.className}`}
     >
       <SecurityFeatures />
     </main>
